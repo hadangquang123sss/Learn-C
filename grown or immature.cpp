07488c0a6b7ce03ebaf7e18774d6c9e91d31397f
hadangquang123sss <79650721+hadangquang123sss@users.noreply.github.com>
2021-03-08 22:34:06 +0700

    Add files via upload

---
+++ grown or immature.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+	int age;
+	printf("enter your age :");
+	scanf("%d",&age);
+	if(age >=18)
+	{
+		printf("you have grown");
+	}
+	else{
+		printf("you are immature");
+		}
+		
+
+ return 0 ;
+}
+
